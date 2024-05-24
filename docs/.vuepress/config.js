0c34b3df9e00d04a56e4ace1589bb1902b64b328
2079735781 <2079735781@qq.com>
2024-05-24 22:53:02 +0800

    提交文件

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  port: '8099',
+  title: '博客',
+  description: '欢迎来到我的博客'
+}
+
+module.exports = {
+  themeConfig: {
+    nav: [
+      {
+        text: 'Languages',
+        items: [
+          { text: 'Chinese', link: '/language/chinese' },
+          { text: 'Japanese', link: '/language/japanese' }
+        ]
+      }
+    ]
+  }
+}
